ae67638b4c3b071e2589442391f11bbb294a1730
Chandrashekar-31 <chandrashekarchandu531@gmail.com>
2025-08-24 21:30:16 +0530

    Create data_cleaning.sql

---
+++ data_cleaning.sql
@@ -0,0 +1,11 @@
+-- Cleaning Item_Fat_Content field
+UPDATE blinkit_data
+SET Item_Fat_Content = 
+    CASE 
+        WHEN Item_Fat_Content IN ('LF', 'low fat') THEN 'Low Fat'
+        WHEN Item_Fat_Content = 'reg' THEN 'Regular'
+        ELSE Item_Fat_Content
+    END;
+
+-- Check cleaned data
+SELECT DISTINCT Item_Fat_Content FROM blinkit_data;
